e25d699ad137632b377df190d1a73c10ff054a93
GetUp <108660418+KillerWBI@users.noreply.github.com>
2025-04-14 18:54:25 +0200

    we

---
+++ js/task-1.js
@@ -0,0 +1,7 @@
+function makeTransaction (quantity , pricePerDroid) {
+
+const totalPrice = pricePerDroid * quantity;
+
+console.log ("Your ordered " + quantity + " droids worth " + totalPrice + " credits!")
+
+}
